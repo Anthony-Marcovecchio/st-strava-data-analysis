7a0396dd9fa278e5e129344499eb971819917c0f
Anthony Marcovecchio <105751330+Anthony-Marcovecchio@users.noreply.github.com>
2024-03-17 16:18:31 -0400

    Initial Strava Conect

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 import streamlit as st
+from data import strava_oauth_session, get_athlete_data
 
-st.write("TESTING")
+
+def app():
+    # on app start-up, make user authenticate with Strava
+    if "strava_session" not in st.session_state:
+        st.session_state.strava_session = strava_oauth_session()
+
+    if st.session_state.strava_session is not None:
+        data = get_athlete_data(st.session_state.strava_session)
+        st.write(data)
+
+
+app()
